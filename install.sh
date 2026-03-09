db54962f54baafb90182cfa76afc80517bcc0e2e
Daniel Lavrushin <dala@milestone.dk>
2026-03-09 09:59:44 +0100

    fix(services): Suppress stdout and stderr for service start to reduce log clutter

--- install.sh
+++ install.sh
@@ -1838,8 +1838,8 @@ start_service() {
     procd_open_instance
     procd_set_param command \$PROG --config \$CONFIG
     procd_set_param respawn \${respawn_threshold:-3600} \${respawn_timeout:-5} \${respawn_retry:-5}
-    procd_set_param stdout 1
-    procd_set_param stderr 1
+    procd_set_param stdout 0
+    procd_set_param stderr 0
     procd_set_param pidfile /var/run/b4.pid
     procd_close_instance
 }
